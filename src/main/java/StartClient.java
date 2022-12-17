25eeaa35362093f025ae8fdef45e503547a1f8d3
FotiosBistas <83087431+FotiosBistas@users.noreply.github.com>
2022-12-17 03:38:19 +0200

    Changed help text

--- src/main/java/StartClient.java
+++ src/main/java/StartClient.java
@@ -2,20 +2,20 @@
 public class StartClient {
 
     private static final String helpText = "CLIENT TOOL \n"
-            + "CLIENT MUST BE PROVIDED WITH FOUR POSITIONAL ARGUMENTS \n"
-            + "-SERVER A IP\n"
-            + "-SERVER B IP\n"
-            + "-NUMBER OF FILES TO REQUEST EACH TIME FROM SERVER A\n"
-            + "-NUMBER OF FILES TO REQUEST EACH TIME FROM SERVER B\n"
-            + "-SERVER A PORT\n"
-            + "-SERVER B PORT\n";
+            + "CLIENT MUST BE PROVIDED WITH 3 POSITIONAL ARGUMENTS \n"
+            + "-SERVER IP\n"
+            + "-NUMBER OF FILES TO REQUEST EACH TIME FROM SERVER \n"
+            + "-SERVER PORT\n"
+            + "-STEP IN FILE REQUEST";
 
     public static void main(String[] Args) {
-        if (Args.length < 6) {
+        if (Args.length < 4 || Args.length > 4) {
             System.out.println(helpText);
             return;
         }
-        Client client = new Client(Args[0], Args[1], Integer.parseInt(Args[2]), Integer.parseInt(Args[3]),
-                Integer.parseInt(Args[4]), Integer.parseInt(Args[5]));
+        Client client = new Client(Args[0], Integer.parseInt(Args[1]), Integer.parseInt(Args[2]),
+                Integer.parseInt(Args[3]));
+
+        client.StartRequesting();
     }
 }
