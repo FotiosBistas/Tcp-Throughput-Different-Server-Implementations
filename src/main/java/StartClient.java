a2236b0943f4d83c86f1cf9ec59ccd9ca358b0ff
FotiosBistas <83087431+FotiosBistas@users.noreply.github.com>
2022-12-17 16:33:23 +0200

    Receive metrics from requests

--- src/main/java/StartClient.java
+++ src/main/java/StartClient.java
@@ -1,21 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class StartClient {
 
+    private Long nanoSecondsToSeconds(Long nanoseconds) {
+        return nanoseconds / 1000000000;
+    }
+
+    private Long nanoSecondsToMilliSeconds(Long nanoseconds) {
+        return nanoseconds / 1000000;
+    }
+
     private static final String helpText = "CLIENT TOOL \n"
-            + "CLIENT MUST BE PROVIDED WITH 3 POSITIONAL ARGUMENTS \n"
-            + "-SERVER IP\n"
-            + "-NUMBER OF FILES TO REQUEST EACH TIME FROM SERVER \n"
-            + "-SERVER PORT\n"
-            + "-STEP IN FILE REQUEST";
+            + "CLIENT MUST BE PROVIDED WITH 6 POSITIONAL ARGUMENTS \n"
+            + "-SERVER A IP\n"
+            + "-SERVER B IP\n"
+            + "-SERVER A PORT\n"
+            + "-SERVER B PORT\n"
+            + "-NUMBER OF FILES TO REQUEST FROM SERVER A\n"
+            + "-NUMBER OF FILES TO REQUEST FROM SERVER B\n";
 
     public static void main(String[] Args) {
-        if (Args.length < 4 || Args.length > 4) {
+        if (Args.length < 6 || Args.length > 6) {
             System.out.println(helpText);
             return;
         }
-        Client client = new Client(Args[0], Integer.parseInt(Args[1]), Integer.parseInt(Args[2]),
-                Integer.parseInt(Args[3]));
+        Client client = new Client(Args[0], Args[1], Integer.parseInt(Args[2]), Integer.parseInt(Args[3]),
+                Integer.parseInt(Args[4]), Integer.parseInt(Args[5]));
 
-        client.StartRequesting();
+        ArrayList<ArrayList<Long>> metrics = client.StartRequesting();
+
+        for (ArrayList<Long> metric : metrics) {
+            System.out.println(metric);
+        }
     }
 }
