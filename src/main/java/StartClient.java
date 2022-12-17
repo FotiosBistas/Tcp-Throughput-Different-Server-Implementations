6ae17cca73be73b5ea8455e31f25fadc1dd15f00
FotiosBistas <83087431+FotiosBistas@users.noreply.github.com>
2022-12-17 02:27:35 +0200

    added port number to command line

--- src/main/java/StartClient.java
+++ src/main/java/StartClient.java
@@ -3,16 +3,19 @@ public class StartClient {
 
     private static final String helpText = "CLIENT TOOL \n"
             + "CLIENT MUST BE PROVIDED WITH FOUR POSITIONAL ARGUMENTS \n"
-            + "-NUMBER OF FILES TO RETRIEVE FROM SERVER A\n"
-            + "-NUMBER OF FILES TO RETRIEVE FROM SERVER B\n"
             + "-SERVER A IP\n"
-            + "-SERVER B IP\n";
+            + "-SERVER B IP\n"
+            + "-NUMBER OF FILES TO REQUEST EACH TIME FROM SERVER A\n"
+            + "-NUMBER OF FILES TO REQUEST EACH TIME FROM SERVER B\n"
+            + "-SERVER A PORT\n"
+            + "-SERVER B PORT\n";
 
     public static void main(String[] Args) {
-        if (Args.length < 4) {
+        if (Args.length < 6) {
             System.out.println(helpText);
             return;
         }
-        Client client = new Client(Args[0], Args[1], Integer.parseInt(Args[2]), Integer.parseInt(Args[3]));
+        Client client = new Client(Args[0], Args[1], Integer.parseInt(Args[2]), Integer.parseInt(Args[3]),
+                Integer.parseInt(Args[4]), Integer.parseInt(Args[5]));
     }
 }
