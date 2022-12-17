53d5f50f40c94b460a6478c79aa76749cf2db486
FotiosBistas <83087431+FotiosBistas@users.noreply.github.com>
2022-12-17 02:36:44 +0200

    Request, file name creation
    Request creates a JSON object to be sent over the server.
    
    CreateFileNameFromNumber formats the number given to the command line and creates a filename of type:
    -sXXX where XXX starts from 001 to 999

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,17 +1,26 @@
-
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.net.Socket;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+import org.json.JSONObject;
+
 public class Client {
+
+    private static DataOutputStream dataOutputStream = null;
+    private static DataInputStream dataInputStream = null;
 
     private Socket clientSocket;
 
     private String serverAAddress = null;
     private String serverBAddress = null;
+    private int portA = 0;
+    private int portB = 0;
 
     private int filesNumberServerA = 0;
     private int filesNumberServerB = 0;
+    private int latestFileReceived = 0;
 
     private void log(String message) {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
@@ -20,18 +29,61 @@ public class Client {
     }
 
     public String toString() {
-        return "Client with: \n"
+        return "\nClient with: \n"
                 + "server A address: " + serverAAddress + "\n"
                 + "server B address: " + serverBAddress + "\n"
-                + "retrieve " + filesNumberServerA + " from server A \n"
-                + "retrieve " + filesNumberServerB + " from server B \n";
+                + "request analogy " + filesNumberServerA + " from server A \n"
+                + "request analogy " + filesNumberServerB + " from server B \n"
+                + "server A port: " + portA + "\n"
+                + "server B port: " + portB + "\n";
     }
 
-    public Client(String serverAAddress, String serverBAddress, int filesNumberServerA, int filesNumberServerB) {
+    private void createRequest(String serverIP, int port, int filenumber) {
+        log("Creating new request");
+
+        JSONObject nJsonObject = new JSONObject();
+        String fileRequest = null;
+
+        try {
+            fileRequest = createFileNameFromNumber(filesNumberServerA);
+        } catch (Throwable e) {
+            log("Error while trying to create filename: " + e);
+            return;
+        }
+
+        nJsonObject.put("ServerIP", serverIP);
+        nJsonObject.put("ServerPort", port);
+        nJsonObject.put("Filename", fileRequest);
+
+        log("Created new JSON object: " + nJsonObject);
+    }
+
+    private String createFileNameFromNumber(int number) {
+        String formatted = null;
+
+        if (number < 1 || number > 999) {
+            throw new Error("Out of range number was given");
+        }
+
+        formatted = String.format("%03d", number);
+
+        if (formatted == null) {
+            throw new Error("Formated file number remained null after string format");
+        }
+
+        return "s" + formatted;
+    }
+
+    public Client(String serverAAddress, String serverBAddress, int filesNumberServerA, int filesNumberServerB,
+            int portA, int portB) {
+
         this.serverAAddress = serverAAddress;
         this.serverBAddress = serverBAddress;
         this.filesNumberServerA = filesNumberServerA;
         this.filesNumberServerB = filesNumberServerB;
+        this.portA = portA;
+        this.portB = portB;
+
         log(this.toString());
     }
 }
