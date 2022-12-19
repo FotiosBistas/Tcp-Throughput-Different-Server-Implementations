ca67c23ae8737030d11f65d07eb9dfa2c39df0c0
FotiosBistas <83087431+FotiosBistas@users.noreply.github.com>
2022-12-19 20:05:34 +0200

    broke up the repo into modules

--- server/main/java/Server.java
+++ server/main/java/Server.java
@@ -1,4 +1,3 @@
-package main.java;
 
 import java.io.*;
 import java.net.*;
@@ -14,18 +13,19 @@ public class Server {
     private Socket clientConnection;
 
     // Constructor with specified path to the files
-    public Server(String ip, int servicePort){
+    public Server(String ip, int servicePort) {
         this.ip = ip;
         this.servicePort = servicePort;
 
     }
 
-    private void startServer(){
-        // This method just creates the socket that the server will listen to for requests
+    private void startServer() {
+        // This method just creates the socket that the server will listen to for
+        // requests
         try {
             this.serverSocket = new ServerSocket(this.servicePort);
-            System.out.println("listening for requests at port "+this.servicePort+"...");
-        }catch (IOException e){
+            System.out.println("listening for requests at port " + this.servicePort + "...");
+        } catch (IOException e) {
             e.printStackTrace();
             System.out.println("Error setting up the main.java.Server.");
         }
@@ -33,17 +33,18 @@ public class Server {
         // Loop that accepts connections and creates a thread to
         // TODO: may end up making an implementation with threads.
         try {
-            while (!serverSocket.isClosed()){
+            while (!serverSocket.isClosed()) {
                 clientConnection = serverSocket.accept();
-                /* TODO: Here we can do one of the two:
-                    1) Create a handler Thread that takes care of the requests,
-                    2) The server switches from accepting connections to handling requests
-                        from the established connection.
-                    (note) The later is simpler, and since we only ever accept one connection,
-                     handler threads are not really necessary. It may be a better practice though.
+                /*
+                 * TODO: Here we can do one of the two:
+                 * 1) Create a handler Thread that takes care of the requests,
+                 * 2) The server switches from accepting connections to handling requests
+                 * from the established connection.
+                 * (note) The later is simpler, and since we only ever accept one connection,
+                 * handler threads are not really necessary. It may be a better practice though.
                  */
             }
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
             System.err.println("Error trying to accept a connection.");
             // Close the server socket.
@@ -51,12 +52,12 @@ public class Server {
         }
     }
 
-    private void shutdown(){
+    private void shutdown() {
         try {
-            if(!serverSocket.isClosed()){
+            if (!serverSocket.isClosed()) {
                 serverSocket.close();
             }
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
             System.err.println("Error closing the server Socket.");
         }
@@ -65,30 +66,29 @@ public class Server {
     public static void main(String[] args) {
         // The port & ip address will be provided as command line arguments.
         // If a port number is not provided, port 5096 will be used by default.
-        if (args.length > 2){
+        if (args.length > 2) {
             System.err.println("Too many arguments provided.");
             System.err.println("Valid formats: {IP address, port number} or {IP address}");
         }
-        if (args.length == 0){
-            //System.err.println("Too few arguments provided.");
+        if (args.length == 0) {
+            // System.err.println("Too few arguments provided.");
             System.err.println("You must at least provide an IP address");
         }
         try {
-            if (args.length == 1){
+            if (args.length == 1) {
                 String ip = args[0];
                 Server server = new Server(ip, 5096);
                 server.startServer();
-            }else if (args.length == 2){
+            } else if (args.length == 2) {
                 String ip = args[0];
                 int port = Integer.parseInt(args[1]);
                 Server server = new Server(ip, port);
                 server.startServer();
             }
-        }catch (IllegalArgumentException e){
+        } catch (IllegalArgumentException e) {
             e.printStackTrace();
             System.err.println("Invalid arguments format.");
             System.err.println("Valid formats: {IP address, port number} or {IP address}");
         }
     }
 }
-
